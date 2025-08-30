cde0eaa936f590b991aa32e98291e8043a424f9f
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-30 15:24:00 +0200

    Update settings.js for derohpotato

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;eRMjHQYS#33e7JhmZP6YI3b0rdekBOrk26rnitcFY4gKa1or4QxE",
+  OWNER_NUMBER: "254112820416", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
