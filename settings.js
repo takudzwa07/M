d2f766462fe002bfdc23e1ef02eec3a59321bf7a
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-30 16:48:51 +0200

    Update settings.js for kamalyt1

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;CMUh3JYQ#rwBpR-zB3_cC1AUYnRwYJcEwLMwvKjZU1waN864vDcM",
+  OWNER_NUMBER: "263719647303", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
