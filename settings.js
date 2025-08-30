f85e9b11edda0a50f2aeb5d3f37a084481f4c865
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-30 17:15:23 +0200

    Update settings.js for sagemisimoha

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~745be8fc56239af5497a601baabd66b5473d805c",
+  OWNER_NUMBER: "905369537209", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
