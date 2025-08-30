543c6b71121321cbfbbbe3b4d02e9278f9c848f9
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-30 15:02:43 +0200

    Update settings.js for temz

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~5a8a62bc407fd3d6c6b806ae96bccd7ee56485ee",
+  OWNER_NUMBER: "2347054030542", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
