432efa3054af03eb3af55ddf83cc20f77a951a7c
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-31 06:58:05 +0200

    Update settings.js for Bee

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~cdacad0c3722c246ef5248d418a475d0bd103100",
+  OWNER_NUMBER: "+27 65 142 0523", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
