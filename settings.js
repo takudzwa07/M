0119f28c218f7d1a85d1c84f8186cb9c04c5970e
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-31 14:02:19 +0200

    Update settings.js for sourajit-ai

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~953a6ff8c3393dd198a3bee1481488f8f08deef9",
+  OWNER_NUMBER: "916909950582", 
+  PREFIX: ",",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
