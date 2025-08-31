11bc5248ee27e45c0876f18ac88737e7ed9dc1a5
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-31 17:53:30 +0200

    Update settings.js for subzero-pong

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~5a68f066842a7f566e757cec7bfcc8ee48ab2f3f",
+  OWNER_NUMBER: "254705691318", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
