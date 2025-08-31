8e74a3f748b7f4aed51535d5b9dc6818df18910d
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-31 08:46:57 +0200

    Update settings.js for Subzero-drake3robin

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~4c0c1762190b54d72847bdf1ed9f495f1533573e",
+  OWNER_NUMBER: "254755 163906", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
