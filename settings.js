e2d5fd2a7a69cde0e0b49814731c2a31aa51b7af
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-01 15:10:59 +0200

    Update settings.js for Deep

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~a9a8cb687e4ad0eea64e59fd67f2fa93628466af",
+  OWNER_NUMBER: "8101558336", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
