8c99a5fdbb8cd2730c352ea53e31b86245c6f949
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-01 14:28:19 +0200

    Update settings.js for RICKXO

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~fe100d6c878e986f14e9ffa2b0ce0478e50bb4de",
+  OWNER_NUMBER: "243842932305", 
+  PREFIX: "Robot v2",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
