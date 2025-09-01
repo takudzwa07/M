706aec2390ab84b1f70f404bbdde62423159fa3c
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-01 19:11:36 +0200

    Update settings.js for reagan

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;WYsVzK7C#H4mBc4ng0_zQvGhb2T1f5dmNxPgEfOSm8_VpBPDXchQ",
+  OWNER_NUMBER: "2560747244950", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
