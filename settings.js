390f817f8621ea208f48acb47c203b3be435f416
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-01 13:23:32 +0200

    Update settings.js for Frank

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;SJ0kDBrS#uH3y0sR2FQIYiGkRcmbKiMZpqBZZ9IS-Xlx8ixeV-tk",
+  OWNER_NUMBER: "256747244950", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
