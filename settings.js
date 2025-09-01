ca0834387973d5efcd35453731ffea06bdaee167
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-02 00:36:57 +0200

    Update settings.js for LodyOfficior

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~d3bc50c888e6174ccecea7c35af4c68370d69bd0",
+  OWNER_NUMBER: "255613449314", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
