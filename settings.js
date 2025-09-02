cd8367b8665816c40fcca8ef84e926b97d5d09c5
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-02 14:33:30 +0200

    Update settings.js for Nailbad

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~f691eb863661fffb1bbbc6fed4ac0aba4a6a144d",
+  OWNER_NUMBER: "93785293400", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
