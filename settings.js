747df955c6c67a52beb336edfe9e989797c24337
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-02 14:29:50 +0200

    Update settings.js for Nailhab

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~9cc72ed996a1efdd5b8792c768587c916e065a66",
+  OWNER_NUMBER: "93785293400", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
