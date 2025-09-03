c8af52c17ababefb11e1c1e522940c61fdfa90b2
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-03 23:46:15 +0200

    Update settings.js for Wrea🍀

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "66N5Y54X",
+  OWNER_NUMBER: "263719647303", 
+  PREFIX: "240090183",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
