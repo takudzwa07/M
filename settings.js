50b0db924b3b1e08718f82b2c642e8dc7237a1a3
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-04 16:31:21 +0200

    Update settings.js for gladdyking

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;aZk2BIaY#PLVjTGawnLOTnBeUTGMcf_3VkqWzx9OCPB1lAvvjHMU",
+  OWNER_NUMBER: "263775953409", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
