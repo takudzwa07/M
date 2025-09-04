be8de57c0c58930629e0848d4248dc5af1af368c
takudzwa07 <mrfrankofc17@gmail.com>
2025-09-04 16:37:37 +0200

    Update settings.js for Miku

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~18b52d5b3656d241d5c9bc02bf693ec0b558c170",
+  OWNER_NUMBER: "242064027981", 
+  PREFIX: "✓",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
